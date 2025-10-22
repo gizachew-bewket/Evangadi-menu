40563b32ff9fc264df3a04db06626b976a74becd
Gizachew Bewket <gizachewbewket19@gmail.com>
2025-10-22 08:30:23 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Evangadi-menu/", // <-- MUST be at the top level, not under server
   server: {
     port: 3000,
   },
